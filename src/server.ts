40daaa32b81205dd05d44f4b117aaccc2fb61551
Marcos Felipe <p.marcosfelipe@gmail.com>
2021-06-21 10:23:35 +0100

    add first get and post routes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.get('/test', (request, response) => {
+  return response.send('Hello Get!');
+});
+
+app.post('/test/post', (request, response) => {
+  return response.send('Hello Post!');
+});
+
 app.listen(3000, () => {
   console.log('Server is running!');
 });
