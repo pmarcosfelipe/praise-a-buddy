e8fde36847961e73a39b5cfafb908ebad61b3ade
Marcos Felipe <p.marcosfelipe@gmail.com>
2021-06-23 15:07:59 +0100

    add typeorm and sqlite3 project database

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,6 @@
+import 'reflect-metadata';
 import express from 'express';
 
+import './database';
+
 const app = express();
-
-app.get('/test', (request, response) => {
-  return response.send('Hello Get!');
-});
-
-app.post('/test/post', (request, response) => {
-  return response.send('Hello Post!');
-});
-
-app.listen(3000, () => {
-  console.log('Server is running!');
-});
