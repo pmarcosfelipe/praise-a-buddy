38624f3b402f80283c26b3187f06ee9954f0406d
Marcos Felipe <p.marcosfelipe@gmail.com>
2021-06-23 16:23:25 +0100

    create User service and a method to execute and create a new user on database

--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,5 @@ import express from 'express';
 import './database';
 
 const app = express();
+
+app.listen(3000, () => console.log('server is running...'));
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { UsersRepository } from '../repositories/UsersRepository';
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest) {
+    const usersRepository = new UsersRepository();
+
+    if (!email) {
+      throw new Error('Email incorrect!');
+    }
+
+    const userAlreadyExists = await usersRepository.find({
+      email,
+    });
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists!');
+    }
+
+    const user = usersRepository.create({ name, email, admin });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
