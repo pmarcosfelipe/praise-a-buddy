47d13bb8c0264aefa3a81fd592a06eecb03874ad
Marcos Felipe <p.marcosfelipe@gmail.com>
2021-06-25 11:11:45 +0100

    add jwtwebtoken and refactor user to add password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -4,11 +4,12 @@ import { UsersRepository } from '../repositories/UsersRepository';
 interface IUserRequest {
   name: string;
   email: string;
+  password: string;
   admin?: boolean;
 }
 
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, password, admin }: IUserRequest) {
     const usersRepository = getCustomRepository(UsersRepository);
 
     if (!email) {
@@ -23,7 +24,7 @@ class CreateUserService {
       throw new Error('User already exists!');
     }
 
-    const user = usersRepository.create({ name, email, admin });
+    const user = usersRepository.create({ name, email, password, admin });
     await usersRepository.save(user);
 
     return user;
